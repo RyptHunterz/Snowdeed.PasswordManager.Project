1602f496c7f7ce61397c8262efb732d7c0f84fbb
Stéphane <34942032+RyptHunterz@users.noreply.github.com>
2024-08-22 15:45:58 +0200

    Add IOS Project

---
+++ front/IOS/PasswordManager.IOS/PasswordManager.IOS/PasswordManager_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManager_IOSApp.swift
+//  PasswordManager.IOS
+//
+//  Created by Stephane Ducoroy on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManager_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
